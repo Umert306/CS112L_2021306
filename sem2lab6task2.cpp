043246980f40a8e5853742c79aa42f4f3b369ca7
Umert306 <umertayyab88@gmail.com>
2023-01-24 20:54:51 +0500

    binng

--- sem2lab6task2.cpp
+++ sem2lab6task2.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
-class Officer {
-private :
+class Officer
+{
+private:
     int id;
     string name, rank;
+
 public:
-    static
-    void setter() {
+    static int count;
+    void setter()
+    {
         cout << "Enter Officer ID : ";
         cin >> id;
         cout << "Enter Officer Name : ";
@@ -14,46 +17,54 @@ public:
         cout << "Enter Officer Rank : ";
         cin >> rank;
     }
-    void palindromecheck() {
-        int a,b, pal=0;
+    void palindromecheck()
+    {
+        int a, b, pal = 0;
         b = id;
-        do {
+        do
+        {
             a = b % 10;
             pal = (pal * 10) + a;
             b = b / 10;
         } while (b != 0);
-        //cout << "Comparing " << pal << " with " << id;
-        if (pal != id) {
+        // cout << "Comparing " << pal << " with " << id;
+        if (pal != id)
+        {
             cout << "Officer Allowed to Enter Area !" << endl;
             count++;
         }
         else
             cout << "Entry Denied !" << endl;
-
     }
 };
 int Officer::count;
-int main(){
+int main()
+{
     int choice;
     Officer o1;
-    while (true) {
+    while (true)
+    {
         cout << "1. Enter ID of Officer" << endl;
         cout << "2. Get total Number of Officers" << endl;
         cout << "3. Exit" << endl;
         cin >> choice;
         if (choice == 3)
             break;
-        if ((choice > 3) || (choice < 1)) {
+        if ((choice > 3) || (choice < 1))
+        {
             cout << "Wrong choice entered" << endl;
             continue;
         }
-        switch (choice) {
-        case 1: {
+        switch (choice)
+        {
+        case 1:
+        {
             o1.setter();
             o1.palindromecheck();
             break;
         }
-        case 2: {
+        case 2:
+        {
             cout << "Officers Entered : " << o1.count << endl;
             break;
         }
